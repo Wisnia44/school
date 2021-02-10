7c8a06e23be0e0094058326de48beed8dc6d32d7
Tomek Wiśniewski <tttttttttttttt7@gmail.com>
2021-02-10 17:05:42 +0100

    Fixing sending activation emails for superusers

--- administration/models.py
+++ administration/models.py
@@ -48,10 +48,14 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        user = self.create_user(email,password=password)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
         user.is_staff = True
         user.is_admin = True
         user.save(using=self._db)
+        UserManager.send_activation_link(email,password)
         return user
 
 
